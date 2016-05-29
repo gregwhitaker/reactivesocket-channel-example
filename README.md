ba027dbcab9d2230310dc3447531e7077364afd3
gregwhitaker <gwhitake@gmail.com>
2016-05-29 10:31:59 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+reactivesocket-channel-example
+===
---
+++ src/main/java/com/github/gregwhitaker/rschannel/ExampleRunner.java
@@ -0,0 +1,11 @@
+package com.github.gregwhitaker.rschannel;
+
+public class ExampleRunner {
+
+    public static void main(String... args) throws Exception {
+        Server server1 = new Server();
+        Server server2 = new Server();
+
+        Thread.currentThread().join();
+    }
+}
